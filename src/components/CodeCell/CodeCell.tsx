34e5aff0e04b4daf2e88a13581bb03cd3bd0e5cc
Ev4LiA <dragonflyer1998@gmail.com>
2022-03-03 15:01:52 +0700

    second init

---
+++ src/components/CodeCell/CodeCell.tsx
@@ -0,0 +1,59 @@
+import './CodeCell.css';
+import { useEffect } from 'react';
+import CodeEditor from '../CodeEditor/CodeEditor';
+import Preview from '../Preview/preview';
+import Resizable from '../Resizable/resizable';
+import { Cell } from '../../state';
+import { useActions } from '../../hooks/useActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { useCumulativeCode } from '../../hooks/useCumulativeCode';
+
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
+
+  useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode);
+      return;
+    }
+
+    const timer = setTimeout(async () => {
+      createBundle(cell.id, cumulativeCode);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cumulativeCode, cell.id, createBundle]);
+
+  return (
+    <Resizable direction="vertical">
+      <div className="code-cell">
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
+        </Resizable>
+        {!bundle || bundle.loading ? (
+          <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
