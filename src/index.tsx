c26b7130e1d993d42fa910133de44af8844bc687
Ev4LiA <dragonflyer1998@gmail.com>
2022-03-03 20:39:16 +0700

    changing document title

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import 'bulmaswatch/darkly/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/CellList/CellList';
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'Js - Markdown Sandbox';
+  }, []);
+
   return (
     <Provider store={store}>
       <div>
